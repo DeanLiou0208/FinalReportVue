0a54edf245057ba4171cd326d872d03106b9be80
DeanLiou0208 <ikari0208@gmail.com>
2023-10-06 04:29:15 +0800

    HomePage Shop Login update 2023/10/06

---
+++ eeit168FinalReportVue/src/components/PageSize.vue
@@ -0,0 +1,22 @@
+<template>
+    <select class=" from-select">
+        <option value=0 selected>ALL</option>
+        <option value=5>每頁顯示5筆資料</option>
+        <option value=10>每頁顯示10筆資料</option>
+    </select>    
+</template>
+    
+<script setup >
+    import {ref} from 'vue'
+    const rows = ref(0)
+    const emit = defineEmits(["PageSizeChange"])
+
+        const changeHandler = ()=>{
+            console.log(rows.value)
+            emit("PageSizeChange", rows.value)
+        }
+</script>
+    
+<style scoped>
+    
+</style>
---
+++ eeit168FinalReportVue/src/components/Paging.vue
@@ -0,0 +1,44 @@
+<template>
+    <nav>
+        <ul class="pagination pagination-sm">
+            
+            <li class="page-item" v-for="(value, index) in totalPages" :key="index"
+            @click="chickHandler(value)"
+            >
+            <a :class="{'currentPage' : thePage ===value, 'page-link':true }">{{ value }}</a>
+            </li>
+            
+        </ul>
+    </nav>    
+</template>
+    
+<script setup>
+    //定義父元件傳進來的屬姓名稱
+    const props = defineProps({
+        totalPages : Number,
+        thePage : Number
+    })
+
+    console.log(props.totalPages)
+    console.log(props.thePage)
+
+    //定義父元件傳進來的事件名稱
+    const emit = defineEmits(["childClick"])
+
+    const chickHandler = page=>{
+        //引發父元件定義好的事件,並且把資料傳給父元件的事件綁定的函式
+        emit("childClick",page)
+    }
+
+</script>
+    
+<style scoped>
+    
+.pagination li {
+    cursor: pointer;
+}
+
+.currentPage {
+    background-color: lightblue;
+}
+</style>
